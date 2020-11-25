96542211e77e7e1f6a0787bedfe80ebe5d0f9ddb
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-11-25 16:40:46 -0600

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all.sort_by do |dog|
+            dog.most_employees
+        end
+    end
+
+    def show 
+        @dog = Dog.find(params[:id])
+    end
+
+  
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,9 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+    def self.most_employees
+        self.all.sort_by do |dog|
+            dog.employees.size
+        end
+    end
 end
